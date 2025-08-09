9b2e5934770bcbeb5fc1504104b9a5433d6c29aa
NaYoNaYo0630 <a001@cberi.go.kr>
2025-08-09 14:13:48 +0900

    Delete my_project directory

--- my_project/utils.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import ollama
-
-def check_ollama():
-    try:
-        _ = ollama.list()
-    except Exception as e:
-        st.error("❌ Ollama 서버에 연결할 수 없습니다. 서버가 실행 중인지 확인해 주세요.")
-        raise e
