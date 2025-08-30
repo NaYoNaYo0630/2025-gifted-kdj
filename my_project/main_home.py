14d5b044ef5882ffa0f080edbe26524d43962fb0
NaYoNaYo0630 <a001@cberi.go.kr>
2025-08-30 22:35:07 +0900

    Update main_home.py

--- my_project/main_home.py
+++ my_project/main_home.py
@@ -1,10 +1,10 @@
+import uuid
 import streamlit as st
+
 
 st.set_page_config(page_title="Home")
 
-st.title("AI 토론 시스템")
-
 if st.button("🗣️ 토론하러 가기"):
-    st.switch_page("pages/discuss_battle.py")
-elif st.button("📊 모델 성능 비교하러 가기"):
-    st.switch_page("pages/compare_model.py")
+    st.switch_page("pages/토론으로_물어보기.py")
+elif st.button("모델 성능 비교하러 가기"):
+    st.switch_page("pages/성능_비교.py")
